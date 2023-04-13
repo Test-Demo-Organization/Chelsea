b5a000de35d0562aa630b946c581869754c1ec78
pragyansh12 <pragyansh@linkites.com>
2023-04-13 12:39:25 +0530

    fix(api): users

--- users.js
+++ users.js
@@ -1,3 +1,3 @@
 console.log('users')
 console.log('self-hosted runners are here')
-console.log('self-hosted runner is now listening to jobs')
+console.log('self-hosted runner is now listening to jobs build')
